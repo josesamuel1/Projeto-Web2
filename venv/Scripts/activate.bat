b200b1628c84b4fe045f97d48af5e9fbc8f13c13
José Samuel <josesamuel004@gmail.com>
2024-04-03 12:52:12 -0300

    Commitzada

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\joaob\Documents\P4\WEB\Projeto-Web2\venv
+set VIRTUAL_ENV=C:\Users\ubixu\OneDrive\Desktop\Projeto-Web2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
